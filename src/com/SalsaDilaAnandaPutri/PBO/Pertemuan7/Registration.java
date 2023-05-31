e7321a16e5b0d838da1f17fe233037b358dcde4e
SalsaDilaAnandaPutri <131859595+SalsaDilaAnandaPutri@users.noreply.github.com>
2023-05-31 15:25:18 +0700

    Add files via upload

---
+++ src/com/SalsaDilaAnandaPutri/PBO/Pertemuan7/Registration.java
@@ -0,0 +1,15 @@
+package com.Salsadila.PBO.Pertemuan7;
+
+import javax.swing.*;
+
+public class Registration {
+    private JTextField salsaDilaAnandaPutriTextField;
+    private JTextField a21102006TextField;
+    private JTextField s1TeknikInformatikaTextField;
+    private JTextField a085641619643TextField;
+    private JRadioButton lakiLakiRadioButton;
+    private JRadioButton perempuanRadioButton;
+    private JTextArea textArea1;
+    private JButton saveButton;
+    private JButton clearButton;
+}
